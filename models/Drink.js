3e592d79c73089765abb133f8114af4c3509c224
ehoversten <ehoversten@gmail.com>
2019-05-11 14:51:12 -0400

    added Drink Model, connected and tested mongoose DB with data set

--- models/Drink.js
+++ models/Drink.js
@@ -1,3 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const DrinkSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ingredients: [{
+        liquor: {
+            type: String
+        },
+        amt: {
+            type: Number
+        },
+        postscript: {
+            type: String
+        }
+    }],
+    description: {
+        type: String
+    },
+    notes: {
+        type: String,
+    }, 
+    image: {
+        type: String,
+        // required: true
+    },
+    category: {
+        type: String,
+        // required: true
+    }
+}); 
 
+mongoose.model('drinks', DrinkSchema);
 
+// const Drink = mongoose.model('drinks', DrinkSchema);
+// model.exports = Drink;
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,70 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
+
+// LOAD DATABASE DATA
+require('../models/Drink');
+const Drink = mongoose.model('drinks');
+
+
 router.get('/', (req, res) => {
-    res.send("API Route");
+    res.render('drinks/dashboard');
 });
 
+router.get('/add', (req, res) => {
+    res.render('drinks/add');
+});
+
+router.post('/', (req, res) => {
+    console.log('POST');
+    console.log('**********************');
+    console.log(`Request: ${req.body.name}`);
+    console.log(`Request: ${req.body.description}`);
+
+    // Create Errors Array 
+    let errors = [];
+
+    // Server Side Validation
+    if(!req.body.name){
+        errors.push({text: 'Please Enter and Ingredient'});
+    }
+    if (!req.body.description) {
+        errors.push({ text: 'Enter and Amount' });
+    }
+
+    // if ERRORs exist
+    if(errors.length > 0){
+        res.render('api/add', {
+            errors: errors, 
+            name: req.body.name,
+            description: req.body.description
+        });
+    } else {
+        // console.log('PASSED VALIDATION');
+        // Save new OBJECT to Database
+        const newDrink = {
+            name: req.body.name,
+            description: req.body.description
+        }
+        new Drink(newDrink)
+            .save()
+            // Create Promise
+            .then(idea => {
+                // show flash message
+                // req.flash('success_msg', 'Drink Added');
+                console.log("Drink Saved");
+                // redirect to Ideas List page
+                res.redirect('/api');
+            })
+            .catch(err => { 
+                console.log(err); 
+                res.redirect('/api/add');
+            });
+    }
+
+    res.redirect('/api');
+})
 
 module.exports = router;
 
