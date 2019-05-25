2947f72c6a176106d32776166370411ef1e58bbc
ehoversten <ehoversten@gmail.com>
2019-05-24 20:02:39 -0400

    removed multipart upload from edit form, resolved ability to edit data from db

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const router = express.Router();
+const methodOverride = require('method-override');
 const multer = require('multer');
 
 // Set Storage Engine
@@ -45,6 +46,9 @@ const Drink = mongoose.model('drinks');
 
 require('../models/Ingredient');
 const Ingredient = mongoose.model('Ingredient');
+
+// override with POST having ?_method=DELETE
+router.use(methodOverride('_method'));
 
 
 
@@ -248,21 +252,39 @@ router.get('/:id/edit', (req, res) => {
 // ------------------------------------//
 //         UPDATE DRINK ROUTE          //
 // ------------------------------------//
-router.put('/:id/edit', (req, res) => {
-    Drink.findOneAndUpdate( { _id: req.params.id }, 
-        {
-            name: req.body.name, 
-            description: req.body.description,
-            notes: req.body.note,
-            // image: req.file
-        }, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-            res.redirect('/api');
-        }
-    });
+// router.put('/:id', (req, res) => {
+//     Drink.findByIdAndUpdate( req.params.id, 
+//         {
+//             name: req.body.name, 
+//             description: req.body.description,
+//             notes: req.body.note,
+//             // image: req.body.drink_img
+//         }, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(data);
+//             console.log("**********************");
+//                 console.log(req.body.name);
+//                 console.log("**********************");
+//             res.redirect('/api');
+//         }
+//     });
+// });
+
+router.put('/:id', (req, res) => { 
+    console.log("Hit Edit Route");
+    Drink.findOneAndUpdate( req.params.id, req.body, (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log(data);
+                console.log("**********************");
+                console.log(req.body.name);
+                console.log("**********************");
+                res.redirect('/api');
+            }
+        });
 });
 
 
